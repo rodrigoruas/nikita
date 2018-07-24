2de24be94434676a085274e16c21cd4a15eafe58
Rodrigo Ruas <rodrigoruas2@gmail.com>
2018-07-24 16:30:59 -0300

    Second Section Firs Version

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dib2nyadi'
+  config.api_key = '345988584121794'
+  config.api_secret = 'CXi4Vpa3mW5d0BUpIW7DacMW8ds'
+  config.secure = true
+  config.cdn_subdomain = true
+end
